45a30c7c78fbc0f4c8447ac272760c6588c6c3fe
Matheus Lins <matheuslins9@gmail.com>
2019-07-14 19:46:58 -0300

    Update develop-example.py

--- gestao_comissao/settings/develop-example.py
+++ gestao_comissao/settings/develop-example.py
@@ -2,16 +2,5 @@ from decouple import config
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'gestao_comissao',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
 SECRET_KEY = config('SECRET_KEY', cast=str)
 
