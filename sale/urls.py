c7f78aa2c44d5e521ee55639c70cf5173e8f2ea2
Felipe <felipecmro@hotmail.com>
2019-07-23 22:21:38 -0300

    Create Check Comission

--- sale/urls.py
+++ sale/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import SaleListCreateAPIView, SaleRetrieveUpdateDestroyAPIView
+from .views import SaleListCreateAPIView, SaleRetrieveUpdateDestroyAPIView, check_comission
 
 
 urlpatterns = [
     path('', SaleListCreateAPIView.as_view(), name='sale_list_create'),
     path('<id>', SaleRetrieveUpdateDestroyAPIView.as_view(), name='sale_retrieve_update_destroy'),
+    path('check_comission/', check_comission, name='check_comission'),
 ]
