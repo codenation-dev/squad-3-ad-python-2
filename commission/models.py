fdc932010a90f6f50ebe284e9c9f2a55c03251fd
Matheus Lins <matheus.lins@br.experian.com>
2019-07-27 16:47:40 -0300

    [commission_api] Create commission plans

--- commission/models.py
+++ commission/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Plan(models.Model):
 
-    min_value = models.DecimalField(max_digits=10, decimal_places=2)
+    min_value = models.FloatField()
     lower_percentage = models.FloatField()
     upper_percentage = models.FloatField()
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
