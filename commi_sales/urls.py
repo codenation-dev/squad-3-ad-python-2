e16d4a5e297935a6113806fcbf0b9d38ae4c2a0e
Matheus Lins <matheus.lins@br.experian.com>
2019-07-27 23:34:03 -0300

    [doc] Remove swagger and put builtin doc

--- commi_sales/urls.py
+++ commi_sales/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
+
+from rest_framework.documentation import include_docs_urls
 from django.urls import path
-from rest_framework_swagger.views import get_swagger_view
 from rest_framework import routers
 
 from plan.views import PlanViewSet
 from sale.views import SaleViewSet
 from seller.views import SellerViewSet
 
-schema_view = get_swagger_view(title='API Documentation')
-urlpatterns = [path('admin/', admin.site.urls), path('', schema_view)]
+
+urlpatterns = [
+	path('admin/', admin.site.urls),
+	path('', include_docs_urls(title='Commi Sales API', description='Documentation'))
+]
 
 router = routers.SimpleRouter()
 router.register(r'plans', PlanViewSet)
@@ -16,4 +20,3 @@ router.register(r'sales', SaleViewSet)
 router.register(r'sellers', SellerViewSet)
 
 urlpatterns.extend(router.urls)
-
