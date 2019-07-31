552d3e67a8dc9b362f114bdcf22599dd0269c7bf
unknown <alexandre@shimono.com.br>
2019-07-31 13:17:22 -0300

    replaced tabs by spaces, checked created commission on sale creation

--- sale/tests.py
+++ sale/tests.py
@@ -4,41 +4,44 @@ from django.urls import reverse
 
 from plan.models import Plan
 from seller.models import Seller
+from commission.models import Commission
 
 
 class TestSaleViewSet(APITestCase):
 
-	def setUp(self):
-		self.plan = Plan.objects.create(min_value=15000, lower_percentage=5, upper_percentage=15)
-		data_seller = {
-			'name': "Test Seller",
-			'age': 20,
-			'email': "test_seller@gmail.com",
-			'cpf': "123456789",
-			'plan': self.plan,
-		}
-		self.seller = Seller.objects.create(**data_seller)
-		self.client = APIClient()
+    def setUp(self):
+        self.plan = Plan.objects.create(min_value=15000, lower_percentage=5, upper_percentage=15)
+        data_seller = {
+            'name': "Test Seller",
+            'age': 20,
+            'email': "test_seller@gmail.com",
+            'cpf': "123456789",
+            'plan': self.plan,
+        }
+        self.seller = Seller.objects.create(**data_seller)
+        self.client = APIClient()
 
-	def test_create(self):
-		url = reverse('sale-list')
-		data = {
-			'amount': 10000,
-			'year': 2019,
-			'month': 2,
-			'seller': self.seller.id
-		}
-		response = self.client.post(url, data, format='json')
-		self.assertEqual(response.status_code, 201)
+    def test_create(self):
+        url = reverse('sale-list')
+        data = {
+            'amount': 10000,
+            'year': 2019,
+            'month': 2,
+            'seller': self.seller.id
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 201)
+        commission = Commission.objects.filter(pk=1)[0]
+        self.assertEqual(commission.value,500)  #verifies if the created commission was calculated correctly and stored properly
 
 
 class TestCheckCommissionApiView(APITestCase):
 
-	def setUp(self):
-		self.url = reverse('check_commission')
-		self.client = APIClient()
+    def setUp(self):
+        self.url = reverse('check_commission')
+        self.client = APIClient()
 
-	def test_post(self):
-		data = {"seller": 10, "amount": 1000.65}
-		response = self.client.post(self.url, data, format='json')
-		self.assertEqual(response.status_code, 200)
+    def test_post(self):
+        data = {"seller": 10, "amount": 1000.65}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, 200)
--- seller/tests.py
+++ seller/tests.py
@@ -10,69 +10,69 @@ from .views import ListSellersByCommissionAPIView
 
 class TestSellerViewSet(TestCase):
 
-	def setUp(self):
-		self.plan = Plan.objects.create(min_value=15000, lower_percentage=5, upper_percentage=15)
-		data_seller = {
-			'name': "Test Seller",
-			'age': 20,
-			'email': "test_seller@gmail.com",
-			'cpf': "123456789",
-			'plan': self.plan,
-		}
-		self.seller = Seller.objects.create(**data_seller)
-		self.client = APIClient()
+    def setUp(self):
+        self.plan = Plan.objects.create(min_value=15000, lower_percentage=5, upper_percentage=15)
+        data_seller = {
+            'name': "Test Seller",
+            'age': 20,
+            'email': "test_seller@gmail.com",
+            'cpf': "123456789",
+            'plan': self.plan,
+        }
+        self.seller = Seller.objects.create(**data_seller)
+        self.client = APIClient()
 
-	def test_retrieve(self):
-		response = self.client.get(reverse('seller-detail', kwargs={'pk': self.seller.id}))
-		self.assertEqual(response.status_code, 200)
-		self.assertEqual(self.seller.name, "Test Seller")
+    def test_retrieve(self):
+        response = self.client.get(reverse('seller-detail', kwargs={'pk': self.seller.id}))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.seller.name, "Test Seller")
 
-	def test_list(self):
-		response = self.client.get(reverse('seller-list'))
-		self.assertEqual(response.status_code, 200)
-		assert isinstance(response.data, list)
-		assert isinstance(response.data[0]['commissions'], list)
+    def test_list(self):
+        response = self.client.get(reverse('seller-list'))
+        self.assertEqual(response.status_code, 200)
+        assert isinstance(response.data, list)
+        assert isinstance(response.data[0]['commissions'], list)
 
 
 class TestListSellersByCommissionAPIView(TestCase):
 
-	def setUp(self):
-		self.plan = Plan.objects.create(min_value=15000, lower_percentage=5, upper_percentage=15)
-		data_seller = {
-			'name': "Test Seller",
-			'age': 20,
-			'email': "test_seller@gmail.com",
-			'cpf': "123456789",
-			'plan': self.plan,
-		}
-		self.seller = Seller.objects.create(**data_seller)
-		data_sale = {
-			'amount': 10000,
-			'year': 2019,
-			'month': 2,
-			'seller': self.seller
-		}
-		self.sale = Sale.objects.create(**data_sale)
-		self.client = APIClient()
+    def setUp(self):
+        self.plan = Plan.objects.create(min_value=15000, lower_percentage=5, upper_percentage=15)
+        data_seller = {
+            'name': "Test Seller",
+            'age': 20,
+            'email': "test_seller@gmail.com",
+            'cpf': "123456789",
+            'plan': self.plan,
+        }
+        self.seller = Seller.objects.create(**data_seller)
+        data_sale = {
+            'amount': 10000,
+            'year': 2019,
+            'month': 2,
+            'seller': self.seller
+        }
+        self.sale = Sale.objects.create(**data_sale)
+        self.client = APIClient()
 
-	def test_list(self):
-		response = self.client.get(
-			reverse('ordered_sellers', kwargs={'year': 2019, 'month': 2})
-		)
-		self.assertEqual(response.status_code, 200)
+    def test_list(self):
+        response = self.client.get(
+            reverse('ordered_sellers', kwargs={'year': 2019, 'month': 2})
+        )
+        self.assertEqual(response.status_code, 200)
 
-	def test_get_sum_commission_seller(self):
-		commissions = [
-			{
-				"value": 37,
-				"month": 2,
-				"year": 2019
-			},
-			{
-				"value": 3,
-				"month": 2,
-				"year": 2019
-			}
-		]
-		response = ListSellersByCommissionAPIView().get_sum_commission_seller(commissions, 2019, 2)
-		self.assertEqual(response, 40)
+    def test_get_sum_commission_seller(self):
+        commissions = [
+            {
+                "value": 37,
+                "month": 2,
+                "year": 2019
+            },
+            {
+                "value": 3,
+                "month": 2,
+                "year": 2019
+            }
+        ]
+        response = ListSellersByCommissionAPIView().get_sum_commission_seller(commissions, 2019, 2)
+        self.assertEqual(response, 40)
